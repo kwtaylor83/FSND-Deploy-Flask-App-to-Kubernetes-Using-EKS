f1e10868e69864fbd71c37b1e718df05f1afcfd0
Keith Taylor <kwtaylor83@gmail.com>
2020-10-28 09:33:48 +0100

    change to test changes to buildspec.yml

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("HEALTHY!")
+    return jsonify("Healthy")
 
 
 @APP.route('/auth', methods=['POST'])
